a1037276775c310b027884d332e7b5daa5ddaee4
Uri Stolar <mixtek@gmail.com>
2022-12-11 21:30:07 -0600

    updates media

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -22,6 +22,7 @@ const Template = ({ data, pageContext }) => {
               <div className='card-image'>
                 <figure className='image is-4by3 postImage'>
                   <img
+                    className='postImage'
                     src={image.childImageSharp.gatsbyImageData.images.sources[0].src}
                     srcSet={image.childImageSharp.gatsbyImageData.images.sources[0].srcSet}
                     alt={`Pic for ${title}`} />
